391130c2b3f582f37604a11cad19b4e57965218f
daybet <daybetlee@gmail.com>
2020-11-15 21:34:44 +0800

    moved local git repo

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const ip = require("ip");
+
+const winston = require("./utils/winston");
+
+require("./startups/db");
+require("./startups/routes")(app);
+
+const port = process.env.PORT || 10443;
+app.listen(port, () =>
+  winston.info(`Server started http://${ip.address()}:${port}`)
+);
