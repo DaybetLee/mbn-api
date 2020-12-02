639858763e085e4d7b62b6e3460956bab38f26a1
daybet <daybetlee@gmail.com>
2020-12-02 23:22:05 +0800

    - Troubleshooting CORS

--- middlewares/proxy.js
+++ middlewares/proxy.js
@@ -1,10 +1,7 @@
 module.exports = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  //   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    req.header("access-control-request-headers")
-  );
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
 
   next();
 };
